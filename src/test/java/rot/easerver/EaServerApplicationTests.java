c9c75e4e33495e2010ed8afd2381c787ee001b5a
HaLoomin <148430859+mgjang8428@users.noreply.github.com>
2023-12-05 14:40:38 +0900

    Initial commit

---
+++ src/test/java/rot/easerver/EaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rot.easerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
